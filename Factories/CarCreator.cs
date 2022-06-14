078c475eb183ed564c15d558361bf948ed405e34
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 23:58:13 +0530

    using Microsoft.AspNetCore.Hosting; using Microsoft.Extensions.Configuration; using Microsoft.Extensions.DependencyInjection;

--- Factories/CarCreator.cs
+++ Factories/CarCreator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatternFactoryMethod
+﻿namespace DesignPatternFactoryMethod
 {
     public class CarCreator : VehicleCreator
     {
